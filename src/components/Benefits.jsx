00511b81aa11f7a8fa6b63968a43d0b4bb6a2737
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-05-06 11:39:22 +0100

    benefits icon

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,7 +17,7 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] bg"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
@@ -34,7 +34,7 @@ const Benefits = () => {
                     alt={item.title}
                   />
                   <p className="ml-auto text-xs font-bold tracking-wider uppercase font-code text-n-1">
-                    Explore more
+                    learn more
                   </p>
                   <Arrow />
                 </div>
