95f9b9eab28d79193a0cb27a21b9a20ea0931eba
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-05-08 22:54:13 +0100

    border

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -18,13 +18,15 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]  "
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-                // clipPath: "url(#benefits)",
-              }}
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]  border rounded-sm"
+              // style={
+              //   {
+              //     // backgroundImage: `url(${item.backgroundUrl})`,
+              //     // clipPath: "url(#benefits)",
+              //   }
+              // }
               key={item.id}
-              // style={{ clipPath: "url(#benefits)" }}
+              style={{ clipPath: "url(#benefits)" }}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="mb-5 h5">{item.title}</h5>
