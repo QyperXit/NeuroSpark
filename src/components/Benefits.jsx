6d82a2268f54725de160ceb2d163eccf34cf7017
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-05-08 22:39:49 +0100

    svg

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -18,9 +18,13 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]  "
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+                // clipPath: "url(#benefits)",
+              }}
               key={item.id}
+              // style={{ clipPath: "url(#benefits)" }}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="mb-5 h5">{item.title}</h5>
@@ -43,7 +47,7 @@ const Benefits = () => {
 
               <div
                 className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: "url(#benefits)" }}
+                // style={{ clipPath: "url(#benefits)" }}
               >
                 <div className="absolute inset-0 transition-opacity opacity-0 hover:opacity-10">
                   {item.imageUrl && (
